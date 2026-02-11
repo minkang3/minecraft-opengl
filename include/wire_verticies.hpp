d8d8d544753f08323fab448722904b88008bd495
Min Kang <minkang.am@gmail.com>
2026-02-11 22:57:42 +0000

    Fix: Factor out wire verticies

---
+++ include/wire_verticies.hpp
@@ -0,0 +1,195 @@
+const float LINE_THICKNESS = 0.005f;
+
+float wire_verticies[] = {
+	// top front
+	0.5f, 0.5f, 0.5f,
+	-0.5f, 0.5f, 0.5f,
+	-0.5f, 0.5f, 0.5f - LINE_THICKNESS,
+	-0.5f, 0.5f, 0.5f - LINE_THICKNESS,
+	0.5f, 0.5f, 0.5f - LINE_THICKNESS,
+	0.5f, 0.5f, 0.5f,
+		  
+	// top left
+	-0.5f, 0.5f, 0.5f,
+	-0.5f, 0.5f, -0.5f,
+	-0.5f + LINE_THICKNESS, 0.5f, -0.5f,
+	-0.5f + LINE_THICKNESS, 0.5f, -0.5f,
+	-0.5f + LINE_THICKNESS, 0.5f, 0.5f,
+	-0.5f, 0.5f, 0.5f,
+
+	// top back
+	-0.5f, 0.5f, -0.5f,
+	0.5f, 0.5f, -0.5f,
+	0.5f, 0.5f, -0.5 + LINE_THICKNESS,
+	0.5f, 0.5f, -0.5 + LINE_THICKNESS,
+	-0.5f, 0.5f, -0.5 + LINE_THICKNESS,
+	-0.5f, 0.5f, -0.5f,
+
+	// top right
+	0.5f, 0.5f, -0.5f,
+	0.5f, 0.5f, 0.5f,
+	0.5f - LINE_THICKNESS, 0.5f, 0.5f,
+	0.5f - LINE_THICKNESS, 0.5f, 0.5f,
+	0.5f - LINE_THICKNESS, 0.5f, -0.5f,
+	0.5f, 0.5f, -0.5f,
+
+	// front top
+	0.5f, 0.5f, 0.5f,
+	-0.5f, 0.5f, 0.5f,
+	-0.5f, 0.5f - LINE_THICKNESS, 0.5f,
+	-0.5f, 0.5f - LINE_THICKNESS, 0.5f,
+	0.5f, 0.5f - LINE_THICKNESS, 0.5f,
+	0.5f, 0.5f, 0.5f,
+
+	// front right
+	0.5f, 0.5f, 0.5f,
+	0.5f, -0.5f, 0.5f,
+	0.5f - LINE_THICKNESS, -0.5f, 0.5f,
+	0.5f - LINE_THICKNESS, -0.5f, 0.5f,
+	0.5f - LINE_THICKNESS, 0.5f, 0.5f,
+	0.5f, 0.5f, 0.5f,
+
+	// front bot
+	0.5f, -0.5f, 0.5f,
+	-0.5f, -0.5f, 0.5f,
+	-0.5f, -0.5f + LINE_THICKNESS, 0.5f,
+	-0.5f, -0.5f + LINE_THICKNESS, 0.5f,
+	0.5f, -0.5f + LINE_THICKNESS, 0.5f,
+	0.5f, -0.5f, 0.5f,
+
+	// front left
+	-0.5f, -0.5f, 0.5f,
+	-0.5f, 0.5f, 0.5f,
+	-0.5f + LINE_THICKNESS, 0.5f, 0.5f,
+	-0.5f + LINE_THICKNESS, 0.5f, 0.5f,
+	-0.5f + LINE_THICKNESS, -0.5f, 0.5f,
+	-0.5f, -0.5f, 0.5f,
+
+	// bot forward
+	0.5f, -0.5f, 0.5f,
+	-0.5f, -0.5f, 0.5f,
+	-0.5f, -0.5f, 0.5f - LINE_THICKNESS,
+	-0.5f, -0.5f, 0.5f - LINE_THICKNESS,
+	0.5f, -0.5f, 0.5f - LINE_THICKNESS,
+	0.5f, -0.5f, 0.5f,
+
+	// bot left
+	-0.5f, -0.5f, 0.5f,
+	-0.5f, -0.5f, -0.5f,
+	-0.5f + LINE_THICKNESS, -0.5f, -0.5f,
+	-0.5f + LINE_THICKNESS, -0.5f, -0.5f,
+	-0.5f + LINE_THICKNESS, -0.5f, 0.5f,
+	-0.5f, -0.5f, 0.5f,
+
+	// bot back
+	-0.5f, -0.5f, -0.5f,
+	0.5f, -0.5f, -0.5f,
+	0.5f, -0.5f, -0.5f - LINE_THICKNESS,
+	0.5f, -0.5f, -0.5f - LINE_THICKNESS,
+	-0.5f, -0.5f, -0.5f - LINE_THICKNESS,
+	-0.5f, -0.5f, -0.5f,
+
+	// bot right
+	0.5f, -0.5f, -0.5f,
+	0.5f, -0.5f, 0.5f,
+	0.5f - LINE_THICKNESS, -0.5f, 0.5f,
+	0.5f - LINE_THICKNESS, -0.5f, 0.5f,
+	0.5f - LINE_THICKNESS, -0.5f, -0.5f,
+	0.5f, -0.5f, -0.5f,
+
+	// back top
+	-0.5f, 0.5f, -0.5f,
+	0.5f, 0.5f, -0.5f,
+	0.5f, 0.5f - LINE_THICKNESS, -0.5,
+	0.5f, 0.5f - LINE_THICKNESS, -0.5,
+	-0.5f, 0.5f - LINE_THICKNESS, -0.5,
+	-0.5f, 0.5f, -0.5f,
+
+	// back left
+	-0.5f, 0.5f, -0.5f,
+	-0.5f, -0.5f, -0.5f,
+	-0.5f + LINE_THICKNESS, -0.5f, -0.5f,
+	-0.5f + LINE_THICKNESS, -0.5f, -0.5f,
+	-0.5f + LINE_THICKNESS, 0.5f, -0.5f,
+	-0.5f, 0.5f, -0.5f,
+
+	// back bot
+	-0.5f, -0.5f, -0.5f,
+	0.5f, -0.5f, -0.5f,
+	0.5f, -0.5f + LINE_THICKNESS, -0.5f,
+	0.5f, -0.5f + LINE_THICKNESS, -0.5f,
+	-0.5f, -0.5f + LINE_THICKNESS, -0.5f,
+	-0.5f, -0.5f, -0.5f,
+		  
+	// back right
+	0.5f, -0.5f, -0.5f,
+	0.5f, 0.5f, -0.5f,
+	0.5f - LINE_THICKNESS, 0.5f, -0.5f,
+	0.5f - LINE_THICKNESS, 0.5f, -0.5f,
+	0.5f - LINE_THICKNESS, -0.5f, -0.5f,
+	0.5f, -0.5f, -0.5f,
+
+	// left top
+	-0.5f, 0.5f, 0.5f,
+	-0.5f, 0.5f, -0.5f,
+	-0.5f, 0.5f - LINE_THICKNESS, -0.5f,
+	-0.5f, 0.5f - LINE_THICKNESS, -0.5f,
+	-0.5f, 0.5f - LINE_THICKNESS, 0.5f,
+	-0.5f, 0.5f, 0.5f,
+
+	// left bot
+	-0.5f, -0.5f, 0.5f,
+	-0.5f, -0.5f, -0.5f,
+	-0.5f, -0.5f + LINE_THICKNESS, -0.5f,
+	-0.5f, -0.5f + LINE_THICKNESS, -0.5f,
+	-0.5f, -0.5f + LINE_THICKNESS, 0.5f,
+	-0.5f, -0.5f, 0.5f,
+
+	// left front
+	-0.5f, 0.5f, 0.5f,
+	-0.5f, -0.5f, 0.5f,
+	-0.5f, -0.5f, 0.5f - LINE_THICKNESS,
+	-0.5f, -0.5f, 0.5f - LINE_THICKNESS,
+	-0.5f, 0.5f, 0.5f - LINE_THICKNESS,
+	-0.5f, 0.5f, 0.5f,
+
+	// left back
+	-0.5f, 0.5f,  -0.5f,
+	-0.5f, -0.5f, -0.5f,
+	-0.5f, -0.5f, -0.5f + LINE_THICKNESS,
+	-0.5f, -0.5f, -0.5f + LINE_THICKNESS,
+	-0.5f, 0.5f,  -0.5f + LINE_THICKNESS,
+	-0.5f, 0.5f,  -0.5f,
+
+	// right top
+	0.5f, 0.5f, 0.5f,
+	0.5f, 0.5f, -0.5f,
+	0.5f, 0.5f - LINE_THICKNESS, -0.5f,
+	0.5f, 0.5f - LINE_THICKNESS, -0.5f,
+	0.5f, 0.5f - LINE_THICKNESS, 0.5f,
+	0.5f, 0.5f, 0.5f,
+
+	// right bot
+	0.5f, -0.5f, 0.5f,
+	0.5f, -0.5f, -0.5f,
+	0.5f, -0.5f + LINE_THICKNESS, -0.5f,
+	0.5f, -0.5f + LINE_THICKNESS, -0.5f,
+	0.5f, -0.5f + LINE_THICKNESS, 0.5f,
+	0.5f, -0.5f, 0.5f,
+
+	// right front
+	0.5f, 0.5f, 0.5f,
+	0.5f, -0.5f, 0.5f,
+	0.5f, -0.5f, 0.5f - LINE_THICKNESS,
+	0.5f, -0.5f, 0.5f - LINE_THICKNESS,
+	0.5f, 0.5f, 0.5f - LINE_THICKNESS,
+	0.5f, 0.5f, 0.5f,
+
+	// right back
+	0.5f, 0.5f,  -0.5f,
+	0.5f, -0.5f, -0.5f,
+	0.5f, -0.5f, -0.5f + LINE_THICKNESS,
+	0.5f, -0.5f, -0.5f + LINE_THICKNESS,
+	0.5f, 0.5f,  -0.5f + LINE_THICKNESS,
+	0.5f, 0.5f,  -0.5f,
+};
--- src/render.cpp
+++ src/render.cpp
@@ -1,6 +1,8 @@
 #include <render.hpp>
 #include <cassert>
 
+#include <wire_verticies.hpp>
+
 #include <shader.hpp>
 
 namespace Render
@@ -40,203 +42,6 @@ namespace Render
 	int init_wire_render(BlockRender &render)
 	{
 		unsigned int VAO, VBO;
-		const float LINE_THICKNESS = 0.005f;
-
-		float verticies[] = {
-			// top front
-			0.5f, 0.5f, 0.5f,
-			-0.5f, 0.5f, 0.5f,
-			-0.5f, 0.5f, 0.5f - LINE_THICKNESS,
-			-0.5f, 0.5f, 0.5f - LINE_THICKNESS,
-			0.5f, 0.5f, 0.5f - LINE_THICKNESS,
-			0.5f, 0.5f, 0.5f,
-		  
-			// top left
-			-0.5f, 0.5f, 0.5f,
-			-0.5f, 0.5f, -0.5f,
-			-0.5f + LINE_THICKNESS, 0.5f, -0.5f,
-			-0.5f + LINE_THICKNESS, 0.5f, -0.5f,
-			-0.5f + LINE_THICKNESS, 0.5f, 0.5f,
-			-0.5f, 0.5f, 0.5f,
-
-			// top back
-			-0.5f, 0.5f, -0.5f,
-			0.5f, 0.5f, -0.5f,
-			0.5f, 0.5f, -0.5 + LINE_THICKNESS,
-			0.5f, 0.5f, -0.5 + LINE_THICKNESS,
-			-0.5f, 0.5f, -0.5 + LINE_THICKNESS,
-			-0.5f, 0.5f, -0.5f,
-
-			// top right
-			0.5f, 0.5f, -0.5f,
-			0.5f, 0.5f, 0.5f,
-			0.5f - LINE_THICKNESS, 0.5f, 0.5f,
-			0.5f - LINE_THICKNESS, 0.5f, 0.5f,
-			0.5f - LINE_THICKNESS, 0.5f, -0.5f,
-			0.5f, 0.5f, -0.5f,
-
-			// front top
-			0.5f, 0.5f, 0.5f,
-			-0.5f, 0.5f, 0.5f,
-			-0.5f, 0.5f - LINE_THICKNESS, 0.5f,
-			-0.5f, 0.5f - LINE_THICKNESS, 0.5f,
-			0.5f, 0.5f - LINE_THICKNESS, 0.5f,
-			0.5f, 0.5f, 0.5f,
-
-			// front right
-			0.5f, 0.5f, 0.5f,
-			0.5f, -0.5f, 0.5f,
-			0.5f - LINE_THICKNESS, -0.5f, 0.5f,
-			0.5f - LINE_THICKNESS, -0.5f, 0.5f,
-			0.5f - LINE_THICKNESS, 0.5f, 0.5f,
-			0.5f, 0.5f, 0.5f,
-
-			// front bot
-			0.5f, -0.5f, 0.5f,
-			-0.5f, -0.5f, 0.5f,
-			-0.5f, -0.5f + LINE_THICKNESS, 0.5f,
-			-0.5f, -0.5f + LINE_THICKNESS, 0.5f,
-			0.5f, -0.5f + LINE_THICKNESS, 0.5f,
-			0.5f, -0.5f, 0.5f,
-
-			// front left
-			-0.5f, -0.5f, 0.5f,
-			-0.5f, 0.5f, 0.5f,
-			-0.5f + LINE_THICKNESS, 0.5f, 0.5f,
-			-0.5f + LINE_THICKNESS, 0.5f, 0.5f,
-			-0.5f + LINE_THICKNESS, -0.5f, 0.5f,
-			-0.5f, -0.5f, 0.5f,
-
-			// bot forward
-			0.5f, -0.5f, 0.5f,
-			-0.5f, -0.5f, 0.5f,
-			-0.5f, -0.5f, 0.5f - LINE_THICKNESS,
-			-0.5f, -0.5f, 0.5f - LINE_THICKNESS,
-			0.5f, -0.5f, 0.5f - LINE_THICKNESS,
-			0.5f, -0.5f, 0.5f,
-
-			// bot left
-			-0.5f, -0.5f, 0.5f,
-			-0.5f, -0.5f, -0.5f,
-			-0.5f + LINE_THICKNESS, -0.5f, -0.5f,
-			-0.5f + LINE_THICKNESS, -0.5f, -0.5f,
-			-0.5f + LINE_THICKNESS, -0.5f, 0.5f,
-			-0.5f, -0.5f, 0.5f,
-
-			// bot back
-			-0.5f, -0.5f, -0.5f,
-			0.5f, -0.5f, -0.5f,
-			0.5f, -0.5f, -0.5f - LINE_THICKNESS,
-			0.5f, -0.5f, -0.5f - LINE_THICKNESS,
-			-0.5f, -0.5f, -0.5f - LINE_THICKNESS,
-			-0.5f, -0.5f, -0.5f,
-
-			// bot right
-			0.5f, -0.5f, -0.5f,
-			0.5f, -0.5f, 0.5f,
-			0.5f - LINE_THICKNESS, -0.5f, 0.5f,
-			0.5f - LINE_THICKNESS, -0.5f, 0.5f,
-			0.5f - LINE_THICKNESS, -0.5f, -0.5f,
-			0.5f, -0.5f, -0.5f,
-
-			// back top
-			-0.5f, 0.5f, -0.5f,
-			0.5f, 0.5f, -0.5f,
-			0.5f, 0.5f - LINE_THICKNESS, -0.5,
-			0.5f, 0.5f - LINE_THICKNESS, -0.5,
-			-0.5f, 0.5f - LINE_THICKNESS, -0.5,
-			-0.5f, 0.5f, -0.5f,
-
-			// back left
-			-0.5f, 0.5f, -0.5f,
-			-0.5f, -0.5f, -0.5f,
-			-0.5f + LINE_THICKNESS, -0.5f, -0.5f,
-			-0.5f + LINE_THICKNESS, -0.5f, -0.5f,
-			-0.5f + LINE_THICKNESS, 0.5f, -0.5f,
-			-0.5f, 0.5f, -0.5f,
-
-			// back bot
-			-0.5f, -0.5f, -0.5f,
-			0.5f, -0.5f, -0.5f,
-			0.5f, -0.5f + LINE_THICKNESS, -0.5f,
-			0.5f, -0.5f + LINE_THICKNESS, -0.5f,
-			-0.5f, -0.5f + LINE_THICKNESS, -0.5f,
-			-0.5f, -0.5f, -0.5f,
-		  
-			// back right
-			0.5f, -0.5f, -0.5f,
-			0.5f, 0.5f, -0.5f,
-			0.5f - LINE_THICKNESS, 0.5f, -0.5f,
-			0.5f - LINE_THICKNESS, 0.5f, -0.5f,
-			0.5f - LINE_THICKNESS, -0.5f, -0.5f,
-			0.5f, -0.5f, -0.5f,
-
-			// left top
-			-0.5f, 0.5f, 0.5f,
-			-0.5f, 0.5f, -0.5f,
-			-0.5f, 0.5f - LINE_THICKNESS, -0.5f,
-			-0.5f, 0.5f - LINE_THICKNESS, -0.5f,
-			-0.5f, 0.5f - LINE_THICKNESS, 0.5f,
-			-0.5f, 0.5f, 0.5f,
-
-			// left bot
-			-0.5f, -0.5f, 0.5f,
-			-0.5f, -0.5f, -0.5f,
-			-0.5f, -0.5f + LINE_THICKNESS, -0.5f,
-			-0.5f, -0.5f + LINE_THICKNESS, -0.5f,
-			-0.5f, -0.5f + LINE_THICKNESS, 0.5f,
-			-0.5f, -0.5f, 0.5f,
-
-			// left front
-			-0.5f, 0.5f, 0.5f,
-			-0.5f, -0.5f, 0.5f,
-			-0.5f, -0.5f, 0.5f - LINE_THICKNESS,
-			-0.5f, -0.5f, 0.5f - LINE_THICKNESS,
-			-0.5f, 0.5f, 0.5f - LINE_THICKNESS,
-			-0.5f, 0.5f, 0.5f,
-
-			// left back
-			-0.5f, 0.5f,  -0.5f,
-			-0.5f, -0.5f, -0.5f,
-			-0.5f, -0.5f, -0.5f + LINE_THICKNESS,
-			-0.5f, -0.5f, -0.5f + LINE_THICKNESS,
-			-0.5f, 0.5f,  -0.5f + LINE_THICKNESS,
-			-0.5f, 0.5f,  -0.5f,
-
-			// right top
-			0.5f, 0.5f, 0.5f,
-			0.5f, 0.5f, -0.5f,
-			0.5f, 0.5f - LINE_THICKNESS, -0.5f,
-			0.5f, 0.5f - LINE_THICKNESS, -0.5f,
-			0.5f, 0.5f - LINE_THICKNESS, 0.5f,
-			0.5f, 0.5f, 0.5f,
-
-			// right bot
-			0.5f, -0.5f, 0.5f,
-			0.5f, -0.5f, -0.5f,
-			0.5f, -0.5f + LINE_THICKNESS, -0.5f,
-			0.5f, -0.5f + LINE_THICKNESS, -0.5f,
-			0.5f, -0.5f + LINE_THICKNESS, 0.5f,
-			0.5f, -0.5f, 0.5f,
-
-			// right front
-			0.5f, 0.5f, 0.5f,
-			0.5f, -0.5f, 0.5f,
-			0.5f, -0.5f, 0.5f - LINE_THICKNESS,
-			0.5f, -0.5f, 0.5f - LINE_THICKNESS,
-			0.5f, 0.5f, 0.5f - LINE_THICKNESS,
-			0.5f, 0.5f, 0.5f,
-
-			// right back
-			0.5f, 0.5f,  -0.5f,
-			0.5f, -0.5f, -0.5f,
-			0.5f, -0.5f, -0.5f + LINE_THICKNESS,
-			0.5f, -0.5f, -0.5f + LINE_THICKNESS,
-			0.5f, 0.5f,  -0.5f + LINE_THICKNESS,
-			0.5f, 0.5f,  -0.5f,
-
-
-		};
 
 		glGenVertexArrays(1, &VAO);
 		glGenBuffers(1, &VBO);
@@ -244,7 +49,7 @@ namespace Render
 		glBindVertexArray(VAO);
 
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(wire_verticies), wire_verticies, GL_STATIC_DRAW);
 
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 		glEnableVertexAttribArray(0);
